b2dd4f1ed6a028b696064b9d09241ea037de9552
coolkidwrik <wriksen@students.cs.ubc.ca address>
2023-05-04 08:03:21 +0800

    added additional aesthetic features to the GUI

--- ui/startScreen.py
+++ ui/startScreen.py
@@ -2,7 +2,19 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello World!")
-myLabel.pack()
 
+# sets up all necessary details associated with the current frame
+def set_frame():
+    set_images()
+    root.title("My Show List")
+    root.config(background="#e7abf5")
+
+
+# sets all images, including the icon, associated with the current frame
+def set_images():
+    icon = PhotoImage(file="../images/taiga_logo.png")
+    root.iconphoto(True, icon)
+
+
+set_frame()
 root.mainloop()
